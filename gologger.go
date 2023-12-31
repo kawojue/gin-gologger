5a14601c90019cd2608e6c98ed45b4a5dd4a2a06
Raheem Kawojue <kawojue08@gmail.com>
2023-12-31 14:34:44 +0100

    ..

--- gologger.go
+++ gologger.go
@@ -7,11 +7,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	color.NoColor = false
+}
+
 // It takes "dev" on development mode.
 // It takes "release" on release mode.
 func Logger(mode string) gin.HandlerFunc {
-	color.NoColor = false
-
 	return func(ctx *gin.Context) {
 		var statusColor *color.Color
 
@@ -29,7 +31,7 @@ func Logger(mode string) gin.HandlerFunc {
 		case statusCode < 400:
 			statusColor = color.New(color.FgYellow)
 		case statusCode < 500:
-			statusColor = color.New(color.FgMagenta)
+			statusColor = color.New(color.FgRed)
 		default:
 			statusColor = color.New(color.FgRed)
 		}
